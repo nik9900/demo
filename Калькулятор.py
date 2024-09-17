669b5892337cfc40dca764956d0a886002420969
nik9900 <48990248+nik9900@users.noreply.github.com>
2024-09-17 20:25:43 +0400

    Update Калькулятор.py

--- Калькулятор.py
+++ Калькулятор.py
@@ -1,15 +1,17 @@
 
 z = input("Введите выражение (например, 1 + 2): ")
+
 def calculator(main):
     parts = main.split()
     if len(parts) != 3:
         print("Ошибка: неправильный формат.")
-        return print('Ошибка')
     a, operator, b = parts
-    a = int(a)
-    b = int(b)
+    a=float(a)
+    b=float(b)
+    if a%1!=0 and b%1!=0:
+            return print("Ошибка. Должно быть только целове число")
     if not (1 <= a <= 10) or not (1 <= b <= 10):
-        return print("ОшибкаЧисла должны быть в диапазоне от 1 до 10")
+        return print("Ошибка. Числа должны быть в диапазоне от 1 до 10")
     if operator == '+':
         return a + b
     elif operator == '-':
@@ -20,9 +22,10 @@ def calculator(main):
         if b == 0:
             print("Деление на ноль")
             return
+
         return a / b
     else:
         print("Ошибка")
-        return
-s = calculator(z);
-print("Ответ:", s);
+
+s = int(calculator(z));
+print("Ответ:", s)
